f1ef753eca2a3362340da90acf18ffeb6d0aa0f8
EliasPos <70285863+EliasPos@users.noreply.github.com>
2021-01-19 20:39:27 +0100

    final

---
+++ TotalSum.py
@@ -0,0 +1,10 @@
+#Recieves input from the user and prints out the total sum of that input
+
+total = 0
+
+for i in range(10):
+    n = int(input("Number: "))
+    total += n
+
+
+print("Sum is", total)
